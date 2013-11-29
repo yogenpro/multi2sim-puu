7f1cbee0dcb442d32278d8b8f09ec0c84634070a
Hua Liu <pro.zju@gmail.com>
2013-11-30 03:52:05 +0800

    Essential functions of CPU-write feature.

--- src/mem-system/puu.c
+++ src/mem-system/puu.c
@@ -1,12 +1,13 @@
 #include "memory.h"
 #include "puu.h"
+#include "mod-stack.h"
 
 struct puu_t *puu_create(void)
 {
     struct puu_t *puu;
 
     /* Initialize */
-    puu = xcalloc(1, sizeof(struct puu_t));
+    puu = (struct puu_t)xcalloc(1, sizeof(struct puu_t));
 
     return puu;
 };
@@ -15,4 +16,83 @@ void puu_free(struct puu_t *puu)
 {
     /* TODO: may have to check if buffer cleared. */
     free(puu);
+}
+
+long long puu_access(struct puu_t *puu, enum puu_access_kind_t access_kind,
+	unsigned int addr, int *witness_ptr, struct linked_list_t *event_queue,
+	void *event_queue_item, struct mod_client_info_t *client_info)
+{
+	struct mod_stack_t *stack;
+	int event;
+
+	/* Create module stack with new ID */
+	mod_stack_id++;
+	stack = mod_stack_create(mod_stack_id,
+		mod, addr, ESIM_EV_NONE, NULL);
+
+	/* Initialize */
+	stack->witness_ptr = witness_ptr;
+	stack->event_queue = event_queue;
+	stack->event_queue_item = event_queue_item;
+	stack->client_info = client_info;
+
+	if (access_kind == puu_access_write)
+    {
+        // TODO
+        puu_buffer_append_check(puu, addr);
+        puu->counter++;
+
+        if (puu->counter == puu->counter_threshold)
+        {
+            // TODO: modify stack to agree with actual write data.
+            esim_execute_event(EV_MOD_LOCAL_MEM_STORE, stack);
+        }
+    }
+    else if (access_kind == puu_access_evict)
+    {
+        // TODO
+    }
+
+    return stack->id;
+}
+
+/* Append new entry to the tail of buffer list.
+ */
+void puu_buffer_append(struct puu_t *puu, unsigned int addr)
+{
+    struct puu_buffer_entry_t *new_buffer_entry;
+    struct puu_buffer_t *new_buffer_node;
+
+    new_buffer_entry = (struct puu_buffer_entry_t)xcalloc(1, sizeof(struct puu_buffer_entry_t));
+    new_buffer_entry->addr = addr;
+
+    new_buffer_node = (struct puu_buffer_t)xcalloc(1, sizeof(struct puu_buffer_t));
+    new_buffer_node->entry = new_buffer_entry;
+    new_buffer_node->prev = puu->buffer->tail;
+    new_buffer_node->next = NULL;
+
+    puu->buffer_tail->next = new_buffer_node;
+    puu->buffer_tail = puu->buffer_tail->next;
+}
+
+/* Check if entry with same target address already exists in buffer.
+ */
+void puu_buffer_append_check(struct puu_t *puu, unsigned int addr)
+{
+    struct puu_buffer_t *buffer_node;
+
+    buffer_node = puu->buffer_tail;
+    if (puu->buffer_tail != NULL)
+    {
+        while (buffer_node != NULL)
+        {
+            if (buffer_node->entry->addr == addr)
+            {
+                // Entry of same address found. No need to append.
+                return;
+            }
+            buffer_node = buffer_node->prev;
+        }
+    }
+    puu_buffer_append(puu, addr);
 }
--- src/mem-system/puu.h
+++ src/mem-system/puu.h
@@ -1,6 +1,14 @@
 #ifndef MEM_SYSTEM_PUU_H
 #define MEM_SYSTEM_PUU_H
 
+/* Access type */
+enum puu_access_kind_t
+{
+	puu_access_invalid = 0,
+	puu_access_write,
+	puu_access_evict
+};
+
 struct puu_buffer_entry_t
 {
     unsigned int addr;
@@ -11,8 +19,8 @@ struct puu_buffer_t
 {
     struct puu_buffer_entry_t *entry;
 
-    struct puu_buffer_t *prev;
-    struct puu_buffer_t *next;
+    struct puu_buffer_t *prev; // Elder node.
+    struct puu_buffer_t *next; // Newer node.
 };
 
 struct puu_t
@@ -21,12 +29,18 @@ struct puu_t
     unsigned int counter;
 
     struct puu_buffer_t *buffer;
-    struct puu_buffer_t *buffer_head;
-    struct puu_buffer_t *buffer_tail;
+    struct puu_buffer_t *buffer_head; // Eldest node.
+    struct puu_buffer_t *buffer_tail; // Newest node.
     struct puu_buffer_t *buffer_pivot;
 };
 
 struct puu_t *puu_create(void);
 void puu_free(struct puu_t *puu);
+
+long long puu_access(struct puu_t *puu, enum puu_access_kind_t access_kind,
+	unsigned int addr, int *witness_ptr, struct linked_list_t *event_queue,
+	void *event_queue_item, struct mod_client_info_t *client_info);
+
+void puu_buffer_append(struct puu_buffer_t *puu_buffer, unsigned int addr);
 
 #endif // MEM_SYSTEM_PUU_H
